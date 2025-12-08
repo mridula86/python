b612664f15b1defff108e08eff816423734b0c0e
mridula86 <134632126+mridula86@users.noreply.github.com>
2025-12-08 14:29:18 +0530

    Create simple_interest.py
    a python program to calculate simple interest.

---
+++ simple_interest.py
@@ -0,0 +1,8 @@
+# Program to calculate Simple Interest
+
+p = int(input("Enter Principal : "))
+r = int(input("Enter Rate of Interest : "))
+t = int(input("Enter Time : "))
+
+si = (p * r * t) / 100
+print("Simple Interest is :", si)
